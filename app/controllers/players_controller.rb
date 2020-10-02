b5b12c95cb9e90e296baa47626a4cd39d1a51dec
maosan132 <maosan132@gmail.com>
2020-10-02 15:36:28 -0500

    a set of create, edit, delete actions

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,65 @@
+class PlayersController < ApplicationController
+  def index
+    @players = Player.all
+  end
+
+  def  new
+  end
+  
+  def create 
+    player = Player.new(player_params)
+    # player.first_name = params[:player][:first_name]
+    # player.last_name = params[:player][:last_name]
+    if player.save
+      redirect_to '/players'
+    else
+      flash[:errors] = player.errors.full_messages
+      redirect_to '/players/new'
+    end
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+  
+# def update
+#   player = Player.find(params[:id])
+#     if player.update
+#       flash[:success] = "Object was successfully updated"
+#       redirect_to players
+#     else
+#       flash[:error] = player.errors.full_messages
+#       render 'edit'
+#     end
+# end
+
+  def update
+    player = Player.find(params[:id])
+      if player.update(player_params)
+        flash[:success] = "Object was successfully updated"
+        redirect_to '/players'
+      else
+        flash[:error] = player.errors.full_messages
+        redirect_to '/players/#{player.id}/edit'
+      end
+  end
+
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to '/players'
+  end
+  
+
+  
+  private
+
+  def player_params
+    params.require(:player).permit(:first_name, :last_name)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'players' => 'players#index'
+  get 'players/new' => 'players#new'
+  post 'players' => 'players#create'
+  get 'players/:id' => 'players#show'
+  get 'players/:id/edit' => 'players#edit'
+  patch 'players/:id' => 'players#update'
+  delete 'players/:id' => 'players#destroy'
+end
+
